75d8bcbafc8f1eaa3ad190f5a9b618becb63da52
William (Wgyt) <github@wgyt.tk>
2021-01-13 15:51:05 +0000

    new color

--- public/main.js
+++ public/main.js
@@ -4,7 +4,8 @@ $(function() {
 	var COLORS = [
 		'#e21400', '#91580f', '#f8a700', '#f78b00',
 		'#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-		'#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+		'#3b88eb', '#3824aa', '#a700ff', '#d300e7',
+		'#b30000'
 	];
 
 	// Initialize variables
